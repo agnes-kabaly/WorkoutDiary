4e0598d1b8998feb40d81856fa86e1f44c934b0b
agnes-kabaly <alicet.woods@gmail.com>
2018-11-17 20:25:36 +0100

    objectMapper removed from addCalendar, @ReqParam & @ReqBody added

--- src/main/java/com/alice/WorkoutDiary/controller/CalendarController.java
+++ src/main/java/com/alice/WorkoutDiary/controller/CalendarController.java
@@ -2,14 +2,11 @@ package com.alice.WorkoutDiary.controller;
 
 import com.alice.WorkoutDiary.model.Calendar;
 import com.alice.WorkoutDiary.service.CalendarService;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 @RestController
 public class CalendarController {
@@ -29,12 +26,10 @@ public class CalendarController {
     }
 
     @RequestMapping(value = "/addCalendar", method = RequestMethod.POST, consumes = "application/json")
-    public ResponseEntity<String> addCalendar(@RequestBody Map<String, Object> json) {
+    public ResponseEntity<String> addCalendar(
+            @RequestParam(value = "userId") Integer userId,
+            @RequestBody Calendar calendar) {
         try {
-            ObjectMapper mapper = new ObjectMapper();
-            Calendar calendar = mapper.convertValue(json.get("calendar"), Calendar.class);
-            LinkedHashMap<?, ?> linkedUser = (LinkedHashMap) json.get("userId");
-            Integer userId = mapper.convertValue(linkedUser.get("userId"), Integer.class);
             calendarService.addNewCalendar(calendar);
             calendarService.userCreateNewCalendar(userId, calendar);
             return ResponseEntity.ok().body(String.format("Calendar date added with status: %s", calendar.getState()));
